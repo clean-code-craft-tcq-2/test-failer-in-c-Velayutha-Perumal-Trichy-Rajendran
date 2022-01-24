cabb83914c9d584a4d046b7d4d2a6ecd4780dfe8
Velayuthaperumal <VelayuthaPerumal.TrichyRajendran@in.bosch.com>
2022-01-24 16:01:34 +0530

    asserted test condition for alerter

--- misaligned.c
+++ misaligned.c
@@ -16,20 +16,20 @@ int frameColourMap (){
 	const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 	
-	int i = 0, j = 0, result;
+	int i = 0, j = 0, rowWidth;
 	for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-			result = sprintf (colourMapInfo[i * 5 + j], "%d | %s | %s", i * 5 + j, majorColor[i], minorColor[i]);	
+			rowWidth = sprintf (colourMapInfo[i * 5 + j], "%d | %s | %s", i * 5 + j, majorColor[i], minorColor[i]);	
         }
     }
-	return result;
+	return rowWidth;
 }
 
 int main() {
 	
     int lastRowWidth = frameColourMap();
 	int firstRowWidth = strlen(colourMapInfo[0]);
-	printf ("lastRowWidth %d\t firstRowWidth %d\n", lastRowWidth,firstRowWidth);
+	
     assert(lastRowWidth == firstRowWidth);
 	printColorMap();
     printf("All is well (maybe!)\n");
